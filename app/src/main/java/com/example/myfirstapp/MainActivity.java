9430f15ff2f0b27d81537ae50d2141762b1d83b1
Christian Bilevits <chrillebile@gmail.com>
2019-03-31 13:31:28 +0200

    Add basic compass code

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -23,4 +23,9 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
+    public void startCompass(View view){
+        Intent intent = new Intent(this, CompassActivity.class);
+        startActivity(intent);
+    }
 }
